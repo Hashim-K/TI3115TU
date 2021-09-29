10deea725d82611e496e81f333ad26ca204c7a25
Teus <T.Vercoelen@student.tudelft.nl>
2021-09-29 02:31:35 +0200

    All the stuff for importing google events.

--- project/Schedule.py
+++ project/Schedule.py
@@ -1,11 +1,17 @@
-import random
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import GoogleImport
 from General import *
 
 
-def Add2Schedule(event, time_interval):
+def CreateSchedule():
+    return np.zeros(shape=(number_of_days, number_of_slots)) - 1
+
+
+def Add2Schedule(event):
     blocks = []
-    for Occurrence in event.Occurences:
+    for Occurrence in event.occurrences:
         time1, time2 = Occurrence[0], Occurrence[1]
         if time1[0] == time2[0]:
             block = [time1, time2]
@@ -21,14 +27,34 @@ def Add2Schedule(event, time_interval):
             Schedule[block[0][0]][slot] = event.ID
 
 
-def Display(Schedule,xticks,yticks,number_of_days,number_of_slots):
+def ImportGoogleEvents():
+    google_events = GoogleImport.Import(day_zero, number_of_days)
+    colors = GetColors()
+    color_id = 0
+    for event in google_events:
+        label = str(event[0])
+        time1 = DayAndSlot(event[1], day_zero, time_interval)
+        time2 = DayAndSlot(event[2], day_zero, time_interval)
+        Event(label, colors[color_id], [[time1, time2]])
+        color_id = color_id + 1
+
+
+def AppendEvents():
+    for event in Events:
+        Add2Schedule(event)
+
+
+def Display():
+    xticks = CreateXTicks(day_zero, number_of_days)
+    yticks = ['0:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00', '9:00', '10:00', '11:00', '12:00',
+              '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00']
     axes = plt.gca()
     axes.set_xlim([0, number_of_days])
     axes.set_ylim([0, number_of_slots])
     axes.invert_yaxis()
     plt.xticks(np.arange(number_of_days), xticks, rotation=30)
     plt.yticks(np.arange(0, number_of_slots, step=number_of_slots / 24), yticks)
-    plt.title('Schedule for '+DateReformat(day_zero)+' till '+DateReformat(XDaysLater(day_zero,number_of_days)))
+    plt.title('Schedule for ' + DateFormat(day_zero) + ' till ' + DateFormat(XDaysLater(day_zero, number_of_days)))
 
     for j in range(len(Schedule)):
         for k in range(len(Schedule[j])):
@@ -55,16 +81,28 @@ Labels = []
 
 
 class Event:
-    def __init__(self, Label, Color, Occurrences):
+    def __init__(self, Label, Color, occurrences):
         self.Label = Label
-        self.Occurences = Occurrences
+        self.occurrences = occurrences
         Events.append(self)
         Labels.append(Label)
         Colors.append(Color)
         self.ID = Events.index(self)
 
 
-# Preset Events
+# Main
+day_zero = '2021-09-13'  # The first day shown in the schedule.
+number_of_days = 7  # The amount of days shown in the schedule.
+time_interval = 5  # The bin width of the timeslots.
+number_of_slots = round(24 * 60 / time_interval)  # The number of timeslots in a day.
+
+ImportGoogleEvents()  # Imports the events from the google calendar.
+Schedule = CreateSchedule()  # Creates the empty schedule.
+AppendEvents()  # Appends the events to the schedule.
+Display()  # Displays the schedule.
+
+
+# Stuff that might be useful later.
 '''
 Sleep = Event('Sleep', '#546fa8', [])
 MorningRoutine = Event('Morning Routine', '#8399c9', [])
@@ -76,35 +114,7 @@ Dinner = Event('Dinner', '#86c452', [[[0, 222], [0, 230]],
                                      [[5, 223], [5, 231]],
                                      [[6, 222], [6, 230]]])
 FreeTime = Event('Free Time', '#e6f0e9', [])
-'''
 
-
-
-day_zero = '2021-09-13'
-number_of_days = 10
-time_interval = 1
-number_of_slots = round(24*60/time_interval)
-
-Schedule = CreateSchedule(time_interval,number_of_days,number_of_slots)
-
-google_events = GoogleImport.Import(day_zero,number_of_days)
-
-colors = GetColors()
-color_id = 0
-for event in google_events:
-    label = str(event[0])
-    time1 = DayAndSlot(event[1],day_zero,time_interval)
-    time2 = DayAndSlot(event[2],day_zero,time_interval)
-    Event(label, colors[color_id], [[time1,time2]])
-    color_id = color_id + 1
-
-
-
-
-# This bit creates random times for sleeping and adds it to the schedule along with the morning routine.
-# This bit is to be replaced by user input.
-
-'''
 for day in days:
     i = days.index(day)
     hour1, min1 = random.randint(22, 23), random.randint(0, 59)
@@ -120,25 +130,10 @@ for day in days:
     else:
         Sleep.Occurences.append([[i, hour1, min1], [i + 1, hour2, min2]])
         MorningRoutine.Occurences.append([[i + 1, hour2, min2], [i + 1, hour2, min2 + timemr]])
-'''
 
-# Adding Activities to Schedule.
-for Event in Events:
-    Add2Schedule(Event,time_interval)
-
-# Identifying free blocks in the schedule.
 FreeBlocks = EmptySlots(Schedule,number_of_slots)
 
-# This Part is for demonstrating that the program can identify the empty slots.
-'''
 for FreeBlock in FreeBlocks:
     FreeTime.Occurences.append(FreeBlock)
 Add2Schedule(FreeTime,time_interval)
 '''
-
-
-# Display Schedule
-xticks = CreateXTicks(day_zero,number_of_days)
-yticks = ['0:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00', '9:00', '10:00', '11:00', '12:00',
-         '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00']
-Display(Schedule,xticks,yticks,number_of_days,number_of_slots)
