58d841b1610a0c565b2772c0619324da22fd64c2
Nina Oosterlaar <n.i.m.oosterlaar@student.tudelft.nl>
2021-10-06 19:56:27 +0200

    (hopefully) last changes task

--- project/BackEnd/Task.py
+++ project/BackEnd/Task.py
@@ -1,5 +1,5 @@
 import json
-#from Schedule import Events
+from Schedule import Events
 import os.path
 from dataclasses import dataclass
 from datetime import datetime
@@ -8,12 +8,10 @@ from datetime import datetime
 # @dataclass
 class Task:
 
-    # highest_id = Events[-1].ID
-    highest_id = 6
-    # edit id thingy
+    highest_id = Events[-1].ID
 
     def __init__(self, name: str, description: str, duration: int, priority: int, deadline: str,
-                 repeatable: bool, category: str, preferred: str, plan_on_same: bool, session: int):
+                 repeatable: bool, category: str, preferred: str, plan_on_same: bool, session: int, filename: str):
         self.name = name
         self.description = description
         self.duration = duration
@@ -24,8 +22,14 @@ class Task:
         self.preferred = preferred
         self.plan_on_same = plan_on_same
         self.session = session
-        Task.highest_id += 1
-        self.taskID = Task.highest_id
+        if not os.path.exists(filename) or os.stat(filename).st_size < 4:
+            Task.highest_id += 1
+            self.taskID = Task.highest_id
+        else:
+            with open(filename) as file:
+                task_dict = json.load(file)
+                self.taskID = task_dict[-1]['TaskID'] + 1
+
 
     def __str__(self):
         text_description = f"Task \"{self.name}\" ({self.taskID}): {self.description}.\n"\
@@ -68,7 +72,7 @@ def import_task(filename):
         for tasks in task_dict:
             tasks_list.append(Task(tasks['Name'], tasks['Description'], tasks['Duration'],
                     tasks['Priority'], datetime.fromisoformat(tasks['Deadline']), tasks['Repeatable'],
-                    tasks['Category'], tasks['Preferred'], tasks['Plan_on_same'], tasks['Session']))
+                    tasks['Category'], tasks['Preferred'], tasks['Plan_on_same'], tasks['Session'], filename))
     return tasks_list
 
 
--- project/test/TaskTests.py
+++ project/test/TaskTests.py
@@ -3,11 +3,12 @@ from project.BackEnd.Task import Task, import_task, delete_task
 import filecmp
 from datetime import date, datetime
 import os
+from Schedule import Events
 
 class MyTestCase(unittest.TestCase):
     def test_initialising(self):
         task = Task('Netflix', 'Watching a series', 60, 3, "2021-10-05",
-                    False, 'Free Time', "Morning (8:00-12:00)", True, 3)
+                    False, 'Free Time', "Morning (8:00-12:00)", True, 3, 'nofile')
         self.assertEqual('Netflix', task.name)
         self.assertEqual('Watching a series', task.description)
         self.assertEqual(60, task.duration)
@@ -20,11 +21,11 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(3, task.session)
 
     def test_string(self):
-        task = Task("Title", "Description", 5, 0, "2021-10-08", False, "category 1", "Morning (8:00-12:00)", True, 1)
+        task = Task("Title", "Description", 5, 0, "2021-10-08", False, "category 1", "Morning (8:00-12:00)", True, 1, 'nofile')
         self.assertEqual(f"Task \"Title\" ({task.taskID}): Description.\nDeadline: 2021-10-08, number of sessions: 1, session duration: 5", str(task))
 
     def test_export(self):
-        task = Task("Title", "Description", 5, 0, date(2021, 10, 8), False, "category 1", "Morning (8:00-12:00)", True, 1)
+        task = Task("Title", "Description", 5, 0, date(2021, 10, 8), False, "category 1", "Morning (8:00-12:00)", True, 1, 'nofile')
         task.taskID = 1
         if os.path.exists('test/jsonfiles/FileForExportTesting.json'):
             os.remove('test/jsonfiles/FileForExportTesting.json')
@@ -32,7 +33,7 @@ class MyTestCase(unittest.TestCase):
         self.assertTrue(filecmp.cmp('test/jsonfiles/FileForTestingOne.json', 'test/jsonfiles/FileForExportTesting.json', shallow=False))
 
     def test_import(self):
-        task = Task("Title", "Description", 5, 0, datetime(2021, 10, 8, 0, 0), False, "category 1", "Morning (8:00-12:00)", True, 1)
+        task = Task("Title", "Description", 5, 0, datetime(2021, 10, 8, 0, 0), False, "category 1", "Morning (8:00-12:00)", True, 1, 'nofile')
         self.assertEqual(task.name, import_task('test/jsonfiles/FileForTestingOne.json')[0].name)
         self.assertEqual(task.description, import_task('test/jsonfiles/FileForTestingOne.json')[0].description)
         self.assertEqual(task.duration, import_task('test/jsonfiles/FileForTestingOne.json')[0].duration)
@@ -49,7 +50,7 @@ class MyTestCase(unittest.TestCase):
         with open('test/jsonfiles/FileForTestingOne.json') as file:
             self.assertEqual('[]', file.read())
         task = Task("Title", "Description", 5, 0, date(2021, 10, 8), False, "category 1", "Morning (8:00-12:00)", True,
-                    1)
+                    1, 'nofile')
         task.taskID = 1
         task.export_task('test/jsonfiles/FileForTestingOne.json')
         self.assertTrue(filecmp.cmp('test/jsonfiles/FileForTestingOne.json', 'test/jsonfiles/FileForExportTesting.json', shallow=False))
@@ -58,10 +59,25 @@ class MyTestCase(unittest.TestCase):
         file = open("test/jsonfiles/empty.json", "w")
         file.close()
         task = Task("Title", "Description", 5, 0, date(2021, 10, 8), False, "category 1", "Morning (8:00-12:00)", True,
-                    1)
+                    1, 'nofile')
         task.taskID = 1
         task.export_task("test/jsonfiles/empty.json")
         self.assertTrue(filecmp.cmp('test/jsonfiles/FileForTestingOne.json', "test/jsonfiles/empty.json", shallow=False))
+
+    def test_correct_taskID(self):
+        task = Task("Title", "Description", 5, 0, datetime(2021, 10, 8, 0, 0), False, "category 1",
+                    "Morning (8:00-12:00)", True, 1, 'test/jsonfiles/TestID.json')
+        self.assertEqual(task.taskID, 9)
+        task = Task("Title", "Description", 5, 0, datetime(2021, 10, 8, 0, 0), False, "category 1",
+                    "Morning (8:00-12:00)", True, 1, 'test/jsonfiles/testIDempty.json')
+        self.assertEqual(task.taskID, highest_id = Events[-1].ID + 1)
+        task = Task("Title", "Description", 5, 0, datetime(2021, 10, 8, 0, 0), False, "category 1",
+                    "Morning (8:00-12:00)", True, 1, 'nofile')
+        self.assertEqual(task.taskID, highest_id = Events[-1].ID + 1)
+
+
+
+
 
 
 if __name__ == '__main__':
