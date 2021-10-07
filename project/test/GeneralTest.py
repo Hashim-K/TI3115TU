0b948964eeb69d37e7e50a1083ac18833bd348e2
Nina Oosterlaar <n.i.m.oosterlaar@student.tudelft.nl>
2021-10-07 20:41:25 +0200

    100% test coverage general

--- project/test/GeneralTest.py
+++ project/test/GeneralTest.py
@@ -1,6 +1,6 @@
 import unittest
-from project.BackEnd.General import DaysSince2020, LeapYear,  CheckWhatDay, DayAndSlot, Slot
-from project.BackEnd.General import Slot2Time, XDaysLater, CreateXTicks, EmptySlots, DateFormat
+from project.BackEnd.General import DaysSince2020, LeapYear,  CheckWhatDay, DayAndSlot
+from project.BackEnd.General import Slot2Time, XDaysLater, DateFormat, Slot
 
 
 class MyTestCase(unittest.TestCase):
@@ -34,6 +34,37 @@ class MyTestCase(unittest.TestCase):
 
     def test_DayAndSlot(self):
         self.assertEqual([2, 208], DayAndSlot('06-10-2021,17:21:00', '2021-10-04', 5))
+        self.assertEqual([0, 208], DayAndSlot('04-10-2021,17:21:00', '2021-10-04', 5))
+        self.assertEqual([0, 0], DayAndSlot('04-10-2021,00:00:00', '2021-10-04', 5))
+        self.assertEqual([0, 0], DayAndSlot('04-10-2021,00:00:00', '2021-10-04', 5))
+        self.assertEqual([0, 17], DayAndSlot('04-10-2021,17:00:00', '2021-10-04', 60))
+        self.assertEqual([0, 209], DayAndSlot('04-10-2021,17:23:00', '2021-10-04', 5))
+
+    def test_Slot(self):
+        self.assertEqual(208, Slot('17:21:00', 5))
+        self.assertEqual(0, Slot('00:00:00', 5))
+        self.assertEqual(17, Slot('17:00:00', 60))
+        self.assertEqual(209, Slot('17:23:00', 5))
+
+    def test_Slot2Time(self):
+        self.assertEqual('13:55:00', Slot2Time(167, 5))
+        self.assertEqual('07:35:00', Slot2Time(91, 5))
+        self.assertEqual('15:05:00', Slot2Time(181, 5))
+        self.assertEqual('02:05:00', Slot2Time(125, 1))
+
+
+    def test_DateFormat(self):
+        self.assertEqual('September 23rd', DateFormat('2021-09-23'))
+        self.assertEqual('January 1st', DateFormat('2021-01-01'))
+        self.assertEqual('July 12th', DateFormat('2021-07-12'))
+        self.assertEqual('March 2nd', DateFormat('2021-03-02'))
+
+    # I think the description of XDaysLater is not correct
+    def test_XDaysLater(self):
+        self.assertEqual('2021-7-12', XDaysLater('2021-07-12', 0))
+        self.assertEqual('2020-12-17', XDaysLater('2020-12-12', 5))
+        self.assertEqual('2021-12-12', XDaysLater('2020-12-12', 366))
+        self.assertEqual('2021-12-6', XDaysLater('2021-10-07', 60))
 
 
 if __name__ == '__main__':
