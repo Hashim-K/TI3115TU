c5b2a7927bec88def20d101233073b004bf5bbe6
Nina Oosterlaar <n.i.m.oosterlaar@student.tudelft.nl>
2021-10-20 16:28:31 +0200

    Fixed bug

--- project/gui/task_creation_gui.py
+++ project/gui/task_creation_gui.py
@@ -147,7 +147,7 @@ class TaskCreationWindow(GeneralWindow):
         print(new_task)
 
         # Export Task to Save File
-        Task.Task.export_task(new_task, os.path.join(dirname, "save_file.json"))
+        Task.Task.export_task(new_task, os.path.join(dirname, "../save_file.json"))
         GeneralWindow.raise_event(self.ls_w, 'reload_tasks')
 
         # then close task creation GUI
