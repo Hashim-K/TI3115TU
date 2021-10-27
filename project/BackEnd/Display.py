c388b3cc027dd5d6cb0bb2605ba1372b57fa9cc7
Hashim Karim <hashimkarim168@gmail.com>
2021-10-27 17:39:07 +0200

    Created display function

---
+++ project/BackEnd/Display.py
@@ -0,0 +1,7 @@
+class Display:
+    def __init__(self):
+        self.width = 10
+        self.height = 6
+        self.text_color = 'white'
+        self.background_color = '#303136'
+        self.face_color = '#363940'
