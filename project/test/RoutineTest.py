994d1eca99188d7fd84f6f3f6e540438d84fd775
Nina Oosterlaar <n.i.m.oosterlaar@student.tudelft.nl>
2021-10-28 17:21:18 +0200

    Routine 100% coverage

--- project/test/RoutineTest.py
+++ project/test/RoutineTest.py
@@ -84,6 +84,9 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(routine.routine_id, found.routine_id)
         self.assertEqual(routine.name, found.name)
         self.assertEqual(found.timeslots.times(), routine.timeslots.times())
+        found = find_routine('jsonfiles/FileForTestingRoutinesID.json', 6)
+        self.assertEqual(None, found)
+
 
     def test_delete_all_routines(self):
         copyfile('jsonfiles/FileForTestingRoutinesID.json', 'jsonfiles/copy_file_4.json')
@@ -91,9 +94,25 @@ class MyTestCase(unittest.TestCase):
         self.assertTrue(filecmp.cmp('jsonfiles/copy_file_4.json', 'jsonfiles/TestIDempty.json'))
 
     def test_delete_times(self):
-        
+        time_list = TimeList()
+        time_list.timelist = [[[2, 5], [2, 10]]]
+        time_list.add_time(3, 10, 4, 28)
+        delete_times('jsonfiles/FileForTestingRoutines.json', 1, time_list.times())
+        self.assertTrue(filecmp.cmp('jsonfiles/FileForTestingRoutines.json',
+                                    'jsonfiles/FileForTestingRoutinesDeleteTimes.json'))
+        os.remove('jsonfiles/FileForTestingRoutines.json')
+        time_list = TimeList()
+        time_list.timelist = [[[2, 5], [2, 10]]]
+        time_list.add_time(3, 10, 4, 28)
+        routine = Routine(1, "Example", time_list, 'nofile')
+        routine.export_routine('jsonfiles/FileForTestingRoutines.json')
 
-
+@patch('builtins.print')
+def test_no_file_found(mock_print):
+    import_routine('nofile')
+    mock_print.assert_called_with('File does not exist')
+    delete_routine('no file', 3)
+    mock_print.assert_called_with('File does not exist')
 
 
 
