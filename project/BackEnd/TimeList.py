3b1c7bd5f19a670122950652ad80f5a472938cf7
Nina Oosterlaar <n.i.m.oosterlaar@student.tudelft.nl>
2021-10-28 15:26:21 +0200

    Updated timelist

--- project/BackEnd/TimeList.py
+++ project/BackEnd/TimeList.py
@@ -9,26 +9,27 @@ class TimeList:
     def add_time(self, start_day, start_time, end_day, end_time):
         self.timelist.append([[start_day, start_time], [end_day, end_time]])
 
-    def add_duration(self, start_day, start_time,duration):
+
+    def add_duration(self, start_day, start_time, duration):
         presets = Presets()
-        end_time = int(start_time+(duration-1)%(1440/presets.time_interval))
+        end_time = int(start_time+(duration-1) % (1440/presets.time_interval))
         end_day = start_day+floor((duration-1)/(1440/presets.time_interval))
-        if end_time > 1440/presets.time_interval:
-            end_time -= presets.time_interval
+        if end_time > 1440/presets.time_interval - 1:
+            end_time -= 1440/presets.time_interval
             end_day += 1
-        self.timelist.append([[start_day, start_time], [end_day, end_time]])
+        self.timelist.append([[start_day, start_time], [end_day, int(end_time)]])
 
     def delete_time(self, start_day, start_time, end_day, end_time):
         for time in self.timelist:
-            if time == [[start_day, ], [end_day, end_time]]:
+            if time == [[start_day, start_time], [end_day, end_time]]:
                 self.timelist.remove(time)
 
     def delete_duration(self, start_day, start_time, duration):
         presets = Presets()
         end_time = int(start_time + (duration - 1) % (1440 / presets.time_interval))
         end_day = start_day + floor((duration - 1) / (1440 / presets.time_interval))
-        if end_time > 1440 / presets.time_interval:
-            end_time -= presets.time_interval
+        if end_time > 1440 / presets.time_interval - 1:
+            end_time -= 1440/presets.time_interval
             end_day += 1
         for time in self.timelist:
             if time == [[start_day, start_time], [end_day, end_time]]:
--- project/test/TimeListTest.py
+++ project/test/TimeListTest.py
@@ -1,7 +1,54 @@
 import unittest
+from project.BackEnd.TimeList import TimeList
 
 
-# class MyTestCase(unittest.TestCase):
+class MyTestCase(unittest.TestCase):
+
+    def test_add_time(self):
+        time_list = TimeList()
+        time_list.add_time(2, 5, 2, 10)
+        self.assertEqual([[[2, 5], [2, 10]]], time_list.timelist)
+        time_list.add_time(3, 8, 4, 2)
+        self.assertEqual([[[2, 5], [2, 10]], [[3, 8], [4, 2]]], time_list.timelist)
+
+    def test_add_duration(self):
+        time_list = TimeList()
+        time_list.add_duration(2, 5, 6)
+        self.assertEqual([[[2, 5], [2, 10]]], time_list.timelist)
+        time_list.add_duration(3, 90, 10)
+        self.assertEqual([[[2, 5], [2, 10]], [[3, 90], [4, 3]]], time_list.timelist)
+        time_list.add_duration(3, 90, 1)
+        self.assertEqual([[[2, 5], [2, 10]], [[3, 90], [4, 3]], [[3, 90], [3, 90]]], time_list.timelist)
+        time_list.add_duration(3, 90, 6)
+        self.assertEqual([[[2, 5], [2, 10]], [[3, 90], [4, 3]], [[3, 90], [3, 90]], [[3, 90], [3, 95]]], time_list.timelist)
+        time_list = TimeList()
+        time_list.add_duration(3, 90, 7)
+        self.assertEqual([[[3, 90], [4, 0]]], time_list.timelist)
+
+
+    def test_delete_time(self):
+        time_list = TimeList()
+        time_list.timelist = [[[2, 5], [2, 10]], [[3, 90], [4, 3]], [[3, 90], [3, 90]], [[3, 90], [3, 95]]]
+        time_list.delete_time(3, 90, 4, 3)
+        self.assertEqual([[[2, 5], [2, 10]], [[3, 90], [3, 90]], [[3, 90], [3, 95]]], time_list.times())
+        time_list.delete_time(3, 90, 3, 90)
+        self.assertEqual([[[2, 5], [2, 10]], [[3, 90], [3, 95]]], time_list.times())
+        time_list.delete_time(2, 5, 2, 10)
+        time_list.delete_time(3, 90, 3, 96)
+        self.assertEqual([[[3, 90], [3, 95]]], time_list.times())
+        time_list.delete_time(3, 90, 3, 95)
+        self.assertEqual([], time_list.times())
+
+
+    def test_delete_duration(self):
+        time_list = TimeList()
+        time_list.timelist = [[[2, 5], [2, 10]], [[3, 90], [4, 3]], [[3, 90], [3, 90]], [[3, 90], [3, 95]]]
+        time_list.delete_duration(3, 90, 1)
+        self.assertEqual([[[2, 5], [2, 10]], [[3, 90], [4, 3]], [[3, 90], [3, 95]]], time_list.timelist)
+        time_list.delete_duration(3, 90, 6)
+        self.assertEqual([[[2, 5], [2, 10]], [[3, 90], [4, 3]]], time_list.timelist)
+        time_list.delete_duration(3, 90, 10)
+        self.assertEqual([[[2, 5], [2, 10]]], time_list.timelist)
 
 
 if __name__ == '__main__':
